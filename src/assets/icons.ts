fd539b154c75fd517721f983d4e761fedaf2a865
laurenCho9 <lauren.choux@gmail.com>
2024-10-23 07:30:14 +0900

    add: 포트폴리오 썸네일 이미지 규격 통일 및 이미지 추가 및 정리

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -2,4 +2,5 @@ import icon_disc_dark from "./icons/icon_disc_dark.svg";
 import icon_toggle from "./icons/icon_toggle.svg";
 import icon_emoji from "./icons/icon_emoji.png";
 import icon_github from "./icons/icon_github.png";
-export { icon_disc_dark, icon_toggle, icon_emoji, icon_github };
+import icon_velog from "./icons/icon_velog.png";
+export { icon_disc_dark, icon_toggle, icon_emoji, icon_github, icon_velog };
--- src/assets/images-profile.ts
+++ src/assets/images-profile.ts
@@ -1,3 +1,4 @@
+//public/assets/images 는 .json의 data에 담기는 image라서 분리
 import profile_emoji from "./images/profile/profile_emoji.png";
 import profile_image from "./images/profile/profile_image.jpg";
 export { profile_emoji, profile_image };
